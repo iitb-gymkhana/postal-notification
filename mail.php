0f9bbf366968054e3008785643439b6b748475ba
Varun Patil <radialapps@gmail.com>
2019-06-23 10:10:41 +0000

    Log to database on send

--- mail.php
+++ mail.php
@@ -1,6 +1,19 @@
 <?php
+        session_start();
+
+        require 'config.php';
+        require '../connect.php';
+
+        if (session_id() == '' || !isset($_SESSION) || !isset($_SESSION['user']) || $_SESSION['user'] == '') {
+                header("location:login.php");
+                exit;
+        }
+
+        $user = $_SESSION["user"];
         $email = $_POST["email"];
         $comments = $_POST["comments"];
         
         echo $email."\n".$comments;
+
+        $query = mysqli_query($con, "INSERT into postal_notification(sender,recipient,comments)VALUES('$user','$email','$comments')") or die(mysqli_error($con));
 ?>
